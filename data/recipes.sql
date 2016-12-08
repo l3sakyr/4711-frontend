7d8563b29d861797dc6455e36e4dc0d76c355a90
ck <ck@CK>
2016-12-08 07:38:00 -0800

    modified columns and sql setting

--- data/recipes.sql
+++ data/recipes.sql
@@ -26,108 +26,108 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `recipes`
 --
-
+DROP TABLE IF EXISTS `Recipes`;
 CREATE TABLE `recipes` (
-  `code` int(11) NOT NULL,
+  `code` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
   `description` text,
    PRIMARY KEY (`code`)
  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `recipes`
+--
+
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Refresh', 'Sharpen concentration, increase stamina, and revitalize your senses');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Cloud Nine', 'Lift your spirits');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Energy', 'Invigorate and refresh your mind and body');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Exhale', 'An exhilarating essential oil blend that renews and strengthens');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Citrus Dream', 'For promoting a sense of calmness and positivity');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Tranquility', 'For a deeper, more restful sleep');
+INSERT INTO `recipes` (`name`, `description`) VALUES
+('Unwind', 'Melt away stress and ease tension with this uplifting blend');
+
+
+
 CREATE TABLE `ingredients` (
-  `icode` int(11) NOT NULL, 
+  `icode` int(11) NOT NULL AUTO_INCREMENT, 
   `code` int(11) NOT NULL, 
-  `itemNum` text,
   `name` varchar(20),
   `amount` DECIMAL(7, 2) DEFAULT NULL,
   PRIMARY KEY (`icode`),
   FOREIGN KEY (`code`) REFERENCES `recipes` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `recipes`
---
-
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(0, 'Refresh', 'Sharpen concentration, increase stamina, and revitalize your senses');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(1, 'Cloud Nine', 'Lift your spirits');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(2, 'Energy', 'Invigorate and refresh your mind and body');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(3, 'Exhale', 'An exhilarating essential oil blend that renews and strengthens');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(4, 'Citrus Dream', 'For promoting a sense of calmness and positivity');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(5, 'Tranquility', 'For a deeper, more restful sleep');
-INSERT INTO `recipes` (`code`, `name`, `description`) VALUES
-(6, 'Unwind', 'Melt away stress and ease tension with this uplifting blend');
 
 
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(0, 0, 'item1', 'Spearmint', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(1, 0, 'item2', 'Lavendar', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(2, 0, 'item3', 'Eucalyptus', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(3, 0, 'item4', 'Lemon', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(4, 0, 'item5', 'Rosewood', 0.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(5, 0, 'item6', 'Cedarwood', 0.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(6, 1, 'item1', 'Lavendar',2);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(7, 1, 'item2', 'Ylang ylang', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(8, 1, 'item3', 'Neroli', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(9, 1, 'item4', 'Sandalwood', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(10, 2, 'item1', 'Peppermint', 1.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(11, 2, 'item2', 'Rosemary', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(12, 2, 'item3', 'Lemon', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(13, 2, 'item4', 'Bergamot', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(14, 2, 'item5', 'Basil', 0.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(15, 3, 'item1', 'Eucalyptus', 2);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(16, 3, 'item2', 'Peppermint', 1.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(17, 3, 'item3', 'Rosemary', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(18, 3, 'item4', 'Ginger', 5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(19, 4, 'item1', 'Orange', 2.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(20, 4, 'item2', 'Grapefruit', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(21, 4, 'item3', 'Lemon', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(22, 4, 'item4', 'Neroli', 0.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(23, 5, 'item1', 'Orange', 1.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(24, 5, 'item2', 'Lavendar', 1.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(25, 5, 'item3', 'Marjoram', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(26, 5, 'item4', 'Chamomile', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(27, 6, 'item1', 'Orange', 2);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(28, 6, 'item2', 'Lavendar', 1.5);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(29, 6, 'item3', 'Bergamot', 1);
-INSERT INTO `ingredients` (`icode`, `code`, `itemNum`, `name`, `amount`) VALUES
-(30, 6, 'item4', 'Geranium', 5);
-
-
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item1', 'Spearmint', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item2', 'Lavendar', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item3', 'Eucalyptus', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item4', 'Lemon', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item5', 'Rosewood', 0.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(1, 'item6', 'Cedarwood', 0.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(2, 'item1', 'Lavendar',2);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(2, 'item2', 'Ylang ylang', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(2, 'item3', 'Neroli', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(2, 'item4', 'Sandalwood', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(3, 'item1', 'Peppermint', 1.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(3, 'item2', 'Rosemary', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(3, 'item3', 'Lemon', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(3, 'item4', 'Bergamot', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(3, 'item5', 'Basil', 0.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(4, 'item1', 'Eucalyptus', 2);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(4, 'item2', 'Peppermint', 1.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(4, 'item3', 'Rosemary', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(4, 'item4', 'Ginger', 5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(5, 'item1', 'Orange', 2.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(5, 'item2', 'Grapefruit', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(5, 'item3', 'Lemon', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(5, 'item4', 'Neroli', 0.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(6, 'item1', 'Orange', 1.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(6, 'item2', 'Lavendar', 1.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(6, 'item3', 'Marjoram', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(6, 'item4', 'Chamomile', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(7, 'item1', 'Orange', 2);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(7, 'item2', 'Lavendar', 1.5);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(7, 'item3', 'Bergamot', 1);
+INSERT INTO `ingredients` (`code`, `name`, `amount`) VALUES
+(7, 'item4', 'Geranium', 5);
 --
 -- Indexes for dumped tables
 --
