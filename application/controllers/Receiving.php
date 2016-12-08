e116d8b8942791710bd5df006da1a607262ab2aa
Thach Le <A00926906@ad.bcit.ca>
2016-12-07 16:04:12 -0800

    cleaned up Receiving controller to not contain database methods and moved the methods to the supplies controller. Updated the Receiving controller to work with this update.
    Signed-off-by: Thach Le <A00926906@ad.bcit.ca>

--- application/controllers/Receiving.php
+++ application/controllers/Receiving.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Receiving Controller
- *
- * @author Theresa
  */
 class Receiving extends Application
 {
@@ -39,7 +37,7 @@ class Receiving extends Application
             $this->data['supplies'] = $supplies;
 			
 			// View DB supplies
-			$this->viewSupplies();
+			$this->supplies->viewSupplies();
 			
             $this->render();
             
@@ -84,12 +82,6 @@ class Receiving extends Application
 		
 		//NEW
 		$log .= "<br/> Extra: " . $_GET['id'];
-		/*
-		foreach($_POST as $name => $content) { // Most people refer to $key => $value
-			//echo "The HTML name: $name <br>";
-			$log .= $content;
-		}
-		*/
 		
 		//END NEW
 		
@@ -101,13 +93,9 @@ class Receiving extends Application
 		
 		$myfile = fopen("log.txt", "r") or die("Unable to open file!");
 		echo fread($myfile,filesize("log.txt"));
-		//$transaction = fread($myfile,filesize("log.txt"));
 		fclose($myfile);
-		
-		//$supplies = array ($transaction);
 
-		//$this->data = array_merge($this->data, $supplies);
-		$this->receiving_update_db();
+		$this->supplies->receiving_update_db();
 		$this->render();
 	}
 	
@@ -117,115 +105,4 @@ class Receiving extends Application
 		echo fread($myfile,filesize("log.txt"));
 		fclose($myfile);
 	}
-	
-	// Retrieves and displays everything from the supplies table
-	public function viewSupplies() {
-		//SQL
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "supplies";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} 
-
-		$sql = "SELECT * FROM supplies";
-		$result = $conn->query($sql);
-
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-				echo "<br>code: " . $row["code"]. "<br> name: " . $row["name"]. "<br> description: " . $row["description"] . "<br> receiving_cost: " . $row["receiving_cost"] . "<br> receiving_unit: " . $row["receiving_unit"] . "<br> stocking_unit: " . $row["stocking_unit"] . "<br> quantity: " . $row["quantity"] ."<br>";
-			}
-		} else {
-			echo "0 results";
-		}
-		$conn->close();
-		//SQL//
-	}
-	
-	public function receiving_update_db() {
-		//SQL
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "supplies";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} 
-		
-		// quantity = input of order_quantity * select of regex(receiving_unit)
-		// Retrieves and temporarily stores the value of Receiving Unit.
-		$ru_sql = "SELECT receiving_unit FROM supplies WHERE code <=> " . $_GET['id'] . ";";
-		$result = $conn->query($ru_sql);
-				
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-				echo "<br>RU: " . $row["receiving_unit"]. "<br>  ";
-				$pattern = '/\d+/';
-				preg_match($pattern, $row["receiving_unit"], $matches);
-				echo "<br>RU VAL post regex: " . $matches[0] ."<br>";
-			}
-		} else {
-			echo "0 results";
-		}
-		
-		// Calculates the quantity in terms of stock
-		$quantity = $_GET['receiving_unit/'.$_GET['id']] * $matches[0];
-		echo "<br>Quant: " . $quantity;
-		
-		// Modifies the database value of quantity
-		$sql = "UPDATE supplies SET quantity = quantity + " . $quantity . ";";
-		$result = $conn->query($sql);
-		
-		// Displays the Quantity after the modification
-		$sql = "SELECT quantity from supplies where code <=> " . $_GET['id'] . ";";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-				echo "<br>QQquantity: " . $row["quantity"]. "<br>  ";
-				$pattern = '/\d+/';
-				preg_match($pattern, $row["quantity"], $matches);
-				echo "<br>" . $matches[0] ."<br>";
-			}
-		} else {
-			echo "0 results";
-		}
-		
-		if (mysqli_query($conn, $sql)) {
-			echo "Record updated successfully";
-		} else {
-			echo "Error updating record: " . mysqli_error($conn);
-		}
-		$conn->close();
-		//SQL//
-	}
-	
-	/*
-	// logs the cost for total cost of supplies
-	// adds 1000 for testing purposes
-	public function totalCost() {
-		$cost = $this->getCost();
-		$file2 = fopen("log2.txt","w");
-		fwrite($file2,$cost+1000);
-		fclose($file2);
-		
-		$myfile2 = fopen("log2.txt", "r") or die("Unable to open file!");
-		$total = 0;
-		$total = fread($myfile2,filesize("log2.txt"));
-		
-		echo "Total Price:" . $total;
-		fclose($myfile2);
-	}
-	*/
 }
---
+++ application/logs/log-2016-12-08.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-08 00:45:24 --> Severity: Warning --> mysqli::__construct(): (HY000/1049): Unknown database 'supplies' C:\Apps\XAMPP\htdocs\4711-frontend\application\controllers\Receiving.php 130
