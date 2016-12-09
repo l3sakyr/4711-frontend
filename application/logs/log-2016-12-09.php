8550f79775a3d52aba97815676a43f3dca683790
Thach Le <A00926906@ad.bcit.ca>
2016-12-09 11:44:01 -0800

    pull
    Signed-off-by: Thach Le <A00926906@ad.bcit.ca>

---
+++ application/logs/log-2016-12-09.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-09 20:36:42 --> Severity: Warning --> mysqli::__construct(): (HY000/1049): Unknown database 'a2database' C:\Apps\XAMPP\htdocs\4711-frontend\application\models\Supplies.php 271
