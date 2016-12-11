3b5b1e4454813231afbe8668dc6e00c4e66f2bf3
ck <ck@CK>
2016-12-10 23:16:42 -0800

    Fixed homepage and produce views

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -2,6 +2,6 @@
     <h4>Dashboard</h4><br>
     <p>Spent purchasing inventory: ${totalCost}</p>
     <p>Received from sales: ${totalInc}</p>
-    <p>Number of Products: {supplyCount}</p>
-    <p>Number of Products: {recipeCount}</p>
+    <p>Supply Count: {supplyCount}</p>
+    <p>Recipe Count: {recipeCount}</p>
 </div>
